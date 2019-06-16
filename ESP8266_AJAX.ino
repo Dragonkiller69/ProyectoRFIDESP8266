02c823f9552f43d2c948835992f7e65d46a123bf
Kevin Chinchilla <kchinchillam98@gmail.com>
2019-06-16 00:12:42 -0600

    todo chidori

--- ESP8266_AJAX.ino
+++ ESP8266_AJAX.ino
@@ -6,85 +6,66 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
- 
+
 #include "index.html.h" //Our HTML webpage contents with javascripts
- 
+
 #define LED D4  //On board LED
- 
+
 //SSID and Password of your WiFi router
-const char *ssid = "chapaNFC";
-const char *password = "12345678";
- 
-ESP8266WebServer server(8000); //Server on port 8000
- 
+const char* ssid = "testESP8266";
+const char* password = "123456789";
 
-//==============================================================
-//                  SETUP
-//==============================================================
-
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-  
-  WiFi.mode(WIFI_AP);
-  while(!WiFi.softAP(ssid, password)){
-   Serial.println(".");
-    delay(100);
-  }
-  
-  Serial.print("Iniciado AP ");
-  Serial.println(ssid);
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.softAPIP());
- 
-  //Onboard LED port Direction output
-  pinMode(LED,OUTPUT);
-
-  server.on("/micro2019", handleRoot);      //Which routine to handle at root location. This is display page
-  server.on("/setLED", handleLED);
-  server.on("/leerADC", handleADC);
- 
-  server.begin();                  //Start server
-  Serial.println("HTTP server started");
-  
-}
-//==============================================================
-//                     LOOP
-//==============================================================
-void loop(void){
-  server.handleClient();          //Escucha las peticiones del cliente
-}
+ESP8266WebServer server(80); //Server on port 80
 
 //===============================================================
 // This routine is executed when you open its IP in browser
 //===============================================================
 void handleRoot() {
- String s = MAIN_page; //s sería igual a todo el contenido de index.html.h
- server.send(200, "text/html", s); //Mostramos la pagina en el navegador
- Serial.println(s);
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
 }
- 
+
 void handleADC() {
  int a = analogRead(A0);
  String adcValue = String(a);
  
- server.send(200, "text/plane", adcValue); //Enviamos el valor recogido en el pin A0
+ server.send(200, "text/plane", adcValue); //Send ADC value only to client ajax request
 }
- 
+
 void handleLED() {
- String ledState = "OFF";
- String t_state = server.arg("LEDstate"); //Referencia a xhttp.open("GET", "setLED?LEDstate="+led, true);
- Serial.println(t_state);
- if(t_state == "1")
- {
-  digitalWrite(LED,HIGH); //LED ON
-  ledState = "ON"; //Cambiamos el estado
- }
- else
- {
-  digitalWrite(LED,LOW); //LED OFF
-  ledState = "OFF"; //Se cambia el estado
- }
+  Serial.println("Aqui en agregar");
+ String ledState = "Llave agregada exitosamente.";
+ server.send(200, "text/plane", ledState); //Send web page
+}
+//==============================================================
+//                  SETUP
+//==============================================================
+void setup(void){
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT); 
+  // Conectar al WiFi
+   WiFi.mode(WIFI_AP);
+  while(!WiFi.softAP(ssid, password)){
+   Serial.println(".");
+    delay(100);
+  }
+
+  Serial.print("Iniciado AP ");
+  Serial.println(ssid);
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.softAPIP());
+
  
- server.send(200, "text/plane", ledState); //Envias estado a la pagina
+  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
+  server.on("/agregar", handleLED);
+  server.on("/readADC", handleADC);
+
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+//==============================================================
+//                     LOOP
+//==============================================================
+void loop(void){
+  server.handleClient();          //Handle client requests
 }
--- index.html.h
+++ index.html.h
@@ -1,47 +1,124 @@
 const String MAIN_page = R"=(
 <!DOCTYPE html>
 <html>
+
+<head>
+    <meta charset='utf-8'>
+    <title>Administrador RFID</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+
 <body>
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">Administrar</a>
+        <a href="#">Listar</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+    <h1>Administracion RFID</h1>
+    <div id="agregar">
+        <h2>Agregar nueva llave</h1>
+            <button type="button" onclick="agregar()">Agregar</button>
+            <br>
+            <h2 id="estado"></h2>
+    </div>
 
-<div id="demo">
-<h1>NodeMCU + AJAX, Micro2019</h1>
-	<button type="button" onclick="sendData(1)">LED ON</button>
-	<button type="button" onclick="sendData(0)">LED OFF</button><BR>
-</div>
+    <script>
+        function agregar() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("estado").innerHTML =
+                        this.responseText;
+                }
+            };
+            xhttp.open("GET", "agregar", true);
+            xhttp.send();
+        }
 
-<div>
-	ADC Value is : <span id="ADCValue">NUAY</span><br>
-    LED State is : <span id="LEDState">NUAY</span>
-</div>
-<script>
-function sendData(led) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("LEDState").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "setLED?LEDstate="+led, true);
-  xhttp.send();
-}
-setInterval(function() {
-  // Call a function repetatively with 2 Second interval
-  getData();
-}, 2000); //2000mSeconds update rate
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "100%";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        }
 
-function getData() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("ADCValue").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "readADC", true);
-  xhttp.send();
-}
-</script>
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.body.style.backgroundColor = "white";
+        }
+    </script>
+
+    <style>
+        h1 {
+            text-align: center;
+        }
+        
+        #agregar {
+            text-align: center;
+            border: black 1px solid;
+            width: 20%;
+        }
+        
+        body {
+            font-family: "Lato", sans-serif;
+            transition: background-color .5s;
+        }
+        
+        .sidenav {
+            height: 100%;
+            max-width: 275px;
+            width: 0;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #111;
+            overflow-x: hidden;
+            transition: 0.7s;
+            padding-top: 60px;
+            text-align: center;
+        }
+        
+        .sidenav a {
+            padding: 8px 8px 8px 32px;
+            text-decoration: none;
+            font-size: 25px;
+            color: #818181;
+            display: block;
+            transition: 0.3s;
+        }
+        
+        .sidenav a:hover {
+            color: #f1f1f1;
+        }
+        
+        .sidenav .closebtn {
+            position: absolute;
+            top: 0;
+            right: 25px;
+            font-size: 36px;
+            margin-left: 50px;
+        }
+        
+        @media screen and (max-height: 450px) {
+            .sidenav {
+                padding-top: 15px;
+            }
+            .sidenav a {
+                font-size: 18px;
+            }
+        }
+        
+        @media only screen and (max-width: 375px) {
+            .sidenav {
+                max-width: 100%;
+                transition: 0.5s;
+            }
+        }
+    </style>
 </body>
+
 </html>
+
 )=";
